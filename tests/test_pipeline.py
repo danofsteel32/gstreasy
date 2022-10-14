e53e16cef19fc3d4e409ade33137430e4928c5bf
danofsteel32 <dan@dandavis.dev>
2022-10-14 18:20:41 -0400

    initial working tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -6,65 +6,87 @@ import pytest
 from gstreasy import GstPipeline
 import gstreasy.utils as utils
 
-# leaky
-PIPE_LINE_ARGS = [
-    (False,),
-    (False,),
-    (True,),
-    (True,),
-]
+
+def rand_array(channels: int = 3):
+    return np.random.randint(low=0, high=255, size=(240, 320, channels), dtype=np.uint8)
 
 
-@pytest.mark.parametrize(
-    "leaky",
-    PIPE_LINE_ARGS,
-)
-def test_simple(leaky):
-    simple_cmd = "videotestsrc num-buffers=10 ! fakevideosink"
-    with GstPipeline(simple_cmd, leaky) as pipeline:
+def test_simple():
+    cmd = "videotestsrc num-buffers=1 ! fakevideosink"
+    with GstPipeline(cmd) as pipeline:
         pass
 
 
-@pytest.mark.parametrize(
-    "leaky",
-    PIPE_LINE_ARGS,
-)
-def test_appsink(leaky):
-    appsink_cmd = "videotestsrc num-buffers=10 ! appsink emit-signals=true sync=false"
-    count = 0
-    with GstPipeline(appsink_cmd, leaky) as pipeline:
+def test_appsink_buffers():
+    num_buffers, count = 10, 0
+    cmd = f"videotestsrc num-buffers={num_buffers} ! " "appsink emit-signals=true"
+    with GstPipeline(cmd) as pipeline:
         while pipeline:
             buffer = pipeline.pop()
             if buffer:
                 count += 1
                 assert isinstance(buffer.data, np.ndarray)
-    assert count == 10
+    assert count == num_buffers
 
 
-@pytest.mark.parametrize(
-    "leaky",
-    PIPE_LINE_ARGS,
-)
-def test_tee(leaky):
-    out = Path("/tmp/recording.mp4")
+def test_tee():
+    num_buffers, count = 10, 0
+    out = Path("/tmp/recording.avi")
     out.unlink(missing_ok=True)
-    tee_cmd = """
-        videotestsrc num-buffers=60 ! tee name=t
+    cmd = f"""
+        videotestsrc num-buffers={num_buffers} ! tee name=t
         t. ! queue ! video/x-raw,format=RGB,framerate=60/1
-           ! appsink emit-signals=true sync=false
+           ! appsink emit-signals=true
         t. ! queue ! video/x-raw,format=RGB,framerate=60/1
-           ! videoconvert
-           ! x264enc tune=zerolatency
-           ! mp4mux
-           ! filesink location=/tmp/recording.mp4
+           ! jpegenc ! avimux
+           ! filesink location={str(out)}
     """
     count = 0
-    with GstPipeline(tee_cmd, leaky) as pipeline:
+    with GstPipeline(cmd) as pipeline:
         while pipeline:
             buffer = pipeline.pop()
             if buffer:
                 count += 1
     # Did we get at least 90% of the buffers?
-    assert count >= int(60 * 0.90)
-    assert out.exists()
+    assert count == num_buffers
     out.unlink()
+
+
+def test_appsrc_with_caps():
+    num_buffers = 10
+    caps = "video/x-raw,width=320,height=240,framerate=60/1,format=RGB"
+    cmd = f"appsrc caps={caps} emit-signals=true num-buffers={num_buffers} ! fakesink"
+    with GstPipeline(cmd) as pipeline:
+        while pipeline:
+            pipeline.push(rand_array())
+
+
+def test_appsrc_no_caps():
+    num_buffers = 10
+
+    cmd = (
+        f"appsrc emit-signals=true num-buffers={num_buffers} !"
+        "videoconvert ! fakesink"
+    )
+
+    with GstPipeline(cmd) as pipeline:
+        pipeline.set_appsrc_caps(width=320, height=240, framerate=10, format="GRAY8")
+        while pipeline:
+            pipeline.push(rand_array(channels=1))
+
+def test_appsrc_and_sink():
+    num_buffers, count = 10, 0
+
+    cmd = (
+        f"appsrc emit-signals=true num-buffers={num_buffers} ! "
+        "appsink emit-signals=true"
+    )
+    with GstPipeline(cmd) as pipeline:
+        pipeline.set_appsrc_caps(width=320, height=240, framerate=10, format="GRAY8")
+        while pipeline:
+            pipeline.push(rand_array(channels=3))
+            buffer = pipeline.pop()
+            if buffer:
+                assert buffer.data.shape == (240, 320)
+                count += 1
+        assert count == num_buffers
