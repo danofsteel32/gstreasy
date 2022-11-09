8a0c22d22290e6c1b4422ee692b00747914a3466
danofsteel32 <dan@dandavis.dev>
2022-11-09 09:11:30 -0500

    better example

--- examples/webcam.py
+++ examples/webcam.py
@@ -12,11 +12,15 @@ logging.basicConfig(level=logging.DEBUG, handlers=[log_handler])
 log = logging.getLogger(__name__)
 
 # Configure pipeline command
-# default format of many webcams is YUY2 but that's not particularly useful in python land
-caps='image/jpeg, width=(int)640, height=(int)480, framerate=(fraction)30/1'
+# videoconvert to RGB for numpy compatibility (webcam default usually YUY2)
+caps='video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)30/1'
 device = int(os.getenv("WEBCAM", 0))
-cmd = f"v4l2src device=/dev/video{device} num-buffers=30 ! {caps} ! jpegdec ! videoconvert ! appsink emit-signals=true"
-# print(cmd)
+cmd = f"""
+    v4l2src device=/dev/video{device}
+      ! {caps} ! videoconvert ! video/x-raw,format=RGB
+      ! appsink emit-signals=true
+"""
+
 with GstPipeline(cmd) as pipeline:
     while pipeline:
         buffer = pipeline.pop()
